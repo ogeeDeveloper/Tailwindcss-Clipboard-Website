60a81d0f0c1d98cc1bfa2a7e3072264d1019400b
ogeeDeveloper <omarogrant202@hotmail.com>
2022-05-31 09:12:44 -0500

    Set up project and added custom medoia queries and colors, also added a google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+  // Set screen sizes
+  screens: {
+    sm: "480px",
+    md: "768px",
+    lg: "976px",
+    xlg: "1440px"
+  },
+    extend: {
+      colors: {
+        strongCyan: "hsl(171, 66%, 44%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)"
+      }
+    },
   },
   plugins: [],
 }
